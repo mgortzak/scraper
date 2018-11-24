dd232379ca264df311516cafa88cf667968fc7b4
Mark Gortzak <mgortzak@gmail.com>
2018-11-24 18:28:59 +0100

    Go to the correct site.

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 const puppeteer = require('puppeteer');
 
 async function getPic() {
+  const baseUri = 'https://www.geldersarchief.nl/bronnen/archieven/?mivast=37&miadt=37&mizig=128&miview=tbl&milang=nl&micols=1&misort=dt%7Casc&mires=0&mip2=pakko*&mip1=bar*&mibj=1954&miej=1954'
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://google.com');
-  await page.setViewport({width: 1000, height: 500})
-  await page.screenshot({path: 'google.png'});
+  await page.goto(baseUri);
+  await page.screenshot({path: 'screenshot.png'});
 
   await browser.close();
 }
